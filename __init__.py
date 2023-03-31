213051f5f2c2d42a521634122b0949b39eb67103
leopard4 <95200973+leopard4@users.noreply.github.com>
2023-03-31 12:11:14 +0900

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-resource_root = os.path.dirname(os.path.abspath(__file__)) # 현재 파일의 경로
-sys.path.append(resource_root) # 현재 파일의 경로를 sys.path에 추가
-
-# 필요한 모듈 가져오기
-import alcohol
